9fe7dd377d520397031a38de7f6dac7dbcacf982
abelevi <abraham.levi.johnson@gmail.com>
2026-01-22 14:49:51 -0700

    Chessboard implementation: including addPiece(), getPiece(), resetBoard() and ChessBoard()

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,5 +53,5 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
-    
+
 }
