2bef86ac5694f1910c0fab4e92873ee8bb048cea
abelevi <abraham.levi.johnson@gmail.com>
2026-03-02 14:18:03 -0700

    ClearHandler

---
+++ server/src/main/java/handler/ClearHandler.java
@@ -0,0 +1,28 @@
+package handler;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import service.ClearService;
+
+import java.util.Map;
+
+public class ClearHandler {
+    private final ClearService clearService;
+    private final Gson gson = new Gson();
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    public void clear(Context ctx) {
+        try {
+            clearService.clear();
+            ctx.status(200);
+            ctx.json(new Object());
+        } catch (DataAccessException e) {
+            ctx.status(500);
+            ctx.json(Map.of("message", e.getMessage()));
+        }
+    }
+}
