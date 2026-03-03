3355be37642c6bb05f5907d63b0f2344aacbcdf3
abelevi <abraham.levi.johnson@gmail.com>
2026-03-02 21:34:33 -0700

    Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,37 @@
 package server;
 
+import dataaccess.*;
+import handler.*;
 import io.javalin.*;
+import service.*;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+
+        UserService userService = new UserService(userDAO, authDAO);
+        GameService gameService = new GameService(gameDAO, authDAO);
+        ClearService clearService = new ClearService(userDAO, authDAO, gameDAO);
+
+        UserHandler userHandler = new UserHandler(userService);
+        GameHandler gameHandler = new GameHandler(gameService);
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
-
+        // TODO: register routes
+        javalin.post("/user", userHandler::register);
+        javalin.post("/session", userHandler::login);
+        javalin.delete("/session", userHandler::logout);
+        javalin.get("/game", gameHandler::listGames);
+        javalin.post("/game", gameHandler::createGame);
+        javalin.put("/game", gameHandler::joinGame);
+        javalin.delete("/db", clearHandler::clear);
     }
 
     public int run(int desiredPort) {
