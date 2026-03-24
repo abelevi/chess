869bd0fed7400e5fa0d5b26889b9f5d914ca01d9
abelevi <abraham.levi.johnson@gmail.com>
2026-03-24 09:40:23 -0600

    register unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,12 +7,14 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -20,10 +22,24 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clear() throws Exception {
+        facade.clear();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerPositive() throws Exception {
+        var auth = facade.register("testuser", "password", "test@email.com");
+        Assertions.assertNotNull(auth.authToken());
+        Assertions.assertEquals("testuser", auth.username());
+    }
+
+    @Test
+    public void registerDuplicate() {
+        Assertions.assertThrows(Exception.class, () -> {
+            facade.register("testuser", "password", "test@email.com");
+            facade.register("testuser", "password", "test@email.com");
+        });
     }
 
 }
